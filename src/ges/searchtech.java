2908192c4b1aeddc67973bf960a856d26c40f3f7
Affan M N M <49243518+affanmohammed@users.noreply.github.com>
2020-07-01 06:51:30 +0530

    Update searchtech.java
    added connection

--- src/ges/searchtech.java
+++ src/ges/searchtech.java
@@ -3,30 +3,55 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.applet.*;
-public class searchtech extends Applet {
+public class searchtech extends JFrame implements ActionListener {
 	private JLabel tech =new JLabel("TECHNICIAN");
+	private JLabel Copy =new JLabel("TECHNICIAN");
 	private JLabel name=new JLabel("Name	:");
 	private JLabel Rating =new JLabel("Rating 	:");
 	private JButton Select = new JButton("SELECT");
-	public void init() 
+	searchtech() 
     {
-  
+  	Color c=new Color(0,153,204);
+	Color d=new Color(255,255,181);
+	getContentPane().setBackground(c);
 
 
         setLayout(null);
-	tech.setBounds(600,50,200,100);
-	tech.setForeground(Color.red);
-	tech.setFont(new Font("Algerian",Font.BOLD,18));
-	name.setBounds(400,150,200,30);
-	name.setForeground(Color.blue);
-	name.setFont(new Font("Arno Pro",Font.BOLD,18));
-	Rating.setBounds(400,200,200,30);
-	Rating.setForeground(Color.blue);
-	Rating.setFont(new Font("Arno Pro",Font.BOLD,18));
-	Select.setBounds(600,250,200,20);
+	tech.setBounds(100,10,500,150);
+	tech.setForeground(d);
+	Copy.setBounds(102,12,500,150);
+	Copy.setForeground(Color.black);
+	tech.setFont(new Font("Algerian",Font.BOLD,40));
+	
+	Copy.setFont(new Font("Algerian",Font.BOLD,40));
+	name.setBounds(100,150,200,30);
+	name.setForeground(Color.white);
+	name.setFont(new Font("Arno Pro",Font.BOLD,25));
+	Rating.setBounds(100,200,200,30);
+	Rating.setForeground(Color.white);
+	Rating.setFont(new Font("Arno Pro",Font.BOLD,25));
+	Select.setBounds(100,250,200,20);
 	add(tech);
 	add(name);
 	add(Rating);
 	add(Select);
+	add(Copy);
+	Select.addActionListener(this);
 }
+public void actionPerformed(ActionEvent p)
+{
+Object q=p.getSource();
+if(q==Select)
+{
+customer_options r=new customer_options(100);
+r.setVisible(true);
+this.setVisible(false);
+r.setSize(750,750);
+}
+}
+public static void main(String[] args){
+        searchtech s = new searchtech();
+        s.setVisible(true);
+        s.setSize(750,750);
+    }
 }
