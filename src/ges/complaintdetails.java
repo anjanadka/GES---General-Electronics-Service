dff92924d6f52f37d4db13971e761b313b8c869c
Affan M N M <49243518+affanmohammed@users.noreply.github.com>
2020-07-01 06:52:50 +0530

    Update complaintdetails.java
    added connection

--- src/ges/complaintdetails.java
+++ src/ges/complaintdetails.java
@@ -3,54 +3,67 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.applet.*;
-public class complaintdetails extends Applet {
+public class complaintdetails extends JFrame implements ActionListener {
 	private Choice EQUIPMENT =new Choice();
 	private Choice Brand =new Choice();
 	private JLabel brand=new JLabel("Brand :");
 	private JLabel head=new JLabel("COMPLAINT DETAILS");
+	private JLabel Copy=new JLabel("COMPLAINT DETAILS");
 
 	private JTextField Complaint=new JTextField(10);
 	private JLabel COMPLAINT =new JLabel("COMPLAINT");
 	private JLabel Equipment =new JLabel("Equipment  :");
 	private JLabel DES =new JLabel("Description :");
 	private JTextField des=new JTextField(100);
-	private JButton Search = new JButton("SEARCH");
+	private JButton Search = new JButton("Search");
+	private JButton back = new JButton("Back");
 
 
 
-    public void init() 
+
+
+    complaintdetails() 
     {
   
 
-	
+	Color c=new Color(0,153,204);
+	Color d=new Color(255,255,181);
+	getContentPane().setBackground(c);
+	Copy.setForeground(Color.black);
         setLayout(null);
-	Equipment.setFont(new Font("Arno Pro",Font.BOLD,18));
-	brand.setFont(new Font("Arno Pro",Font.BOLD,18));
-	head.setFont(new Font("ALGERIAN",Font.BOLD,18));
+	DES.setFont(new Font("Arno Pro",Font.BOLD,25));
+	Equipment.setFont(new Font("Arno Pro",Font.BOLD,25));
+	brand.setFont(new Font("Arno Pro",Font.BOLD,25));
+	head.setFont(new Font("ALGERIAN",Font.BOLD,40));
+	Copy.setFont(new Font("ALGERIAN",Font.BOLD,40));
 	EQUIPMENT.addItem("FAN");
 	EQUIPMENT.addItem("T.V");
 	EQUIPMENT.addItem("Refrigerator");
 	EQUIPMENT.addItem("Washing Machine");
 	EQUIPMENT.addItem("Motor");
 	EQUIPMENT.addItem("Mixer Grinder");
-	EQUIPMENT.setBounds(700,140,200,100);
-	Equipment.setBounds(500,100,200,100);
-	brand.setBounds(500,180,200,100);
-	Brand.setBounds(700,220,200,100);
-	DES.setBounds(500,260,200,100);
-	des.setBounds(700,300,200,20);
+	EQUIPMENT.setBounds(300,140,200,100);
+	Equipment.setBounds(100,100,200,100);
+	brand.setBounds(100,180,200,100);
+	Brand.setBounds(300,220,200,100);
+	DES.setBounds(100,260,200,100);
+	des.setBounds(300,300,200,20);
 	Brand.addItem("BAJAJ");
 	Brand.addItem("WHIRLPOOL");
 	Brand.addItem("SAMSUNG");
 	Brand.addItem("L.G");
 	Brand.addItem("CG");
-	head.setBounds(600,50,200,100);
-	Search.setBounds(600,400,200,20);
-	head.setForeground(Color.red);
-	Equipment.setForeground(Color.blue);
-	DES.setForeground(Color.blue);
-	brand.setForeground(Color.blue);
-	
+	head.setBounds(100,10,500,150);
+	Copy.setBounds(102,12,500,150);
+	Search.setBounds(400,350,80,40);
+	back.setBounds(100,350,80,40);
+	head.setForeground(d);
+	Equipment.setForeground(Color.white);
+	DES.setForeground(Color.white);
+	brand.setForeground(Color.white);
+	Search.addActionListener(this);
+	back.addActionListener(this);
+	add(back);
         add(EQUIPMENT);
 	add(Equipment);
 	add(head);
@@ -60,7 +73,32 @@ public class complaintdetails extends Applet {
 	add(DES);
 	add(des);
 	add(Search);
+	add(Copy);
+}
+public void actionPerformed(ActionEvent k)
+{
+Object l=k.getSource();
+if(l==Search)
+{
+searchtech m=new searchtech();
+m.setVisible(true);
+this.setVisible(false);
+m.setSize(750,750);
+}
+else if(l==back)
+{
+customer_options n=new customer_options(100);
+n.setVisible(true);
+this.setVisible(false);
+n.setSize(750,750);
+}
+
+
 
 }
+    public static void main(String[] args){
+       complaintdetails o = new complaintdetails();
+        o.setVisible(true);
+        o.setSize(750,750);
+    }
 }
-
